57bb3c811d6dd6ad90ccd8eb4297ec3a9ccfa074
Joyce Liao <jliao@stuy.edu>
2017-12-13 19:35:47 -0500

    made toString abstract in Protagonist

--- v3/Protagonist.java
+++ v3/Protagonist.java
@@ -1,4 +1,18 @@
-public class Protagonist extends Character {
+public abstract class Protagonist extends Character {
+
+    /** error message without adding "abstracting" in class header:
+     * Protagonist.java:1: error: Protagonist is not abstract and does not override abstract method toString() in Protagonist
+     */
+
+    /** error message when trying to instantiate a Protagonist instance
+     * Protagonist.java:42: error: Protagonist is abstract; cannot be instantiated
+     */
+
+    /** error message when a subclass of Protagonist does not contain toString
+     * Protagonist.java:46: error: Rogue is not abstract and does not override abstract method toString() in Protagonist
+     */
+
+
     private String name;
 
     protected double default_attack = 0.4;
@@ -28,40 +42,61 @@ public class Protagonist extends Character {
         attack = default_attack;
     }
 	
-    public String toString() {
-        return super.toString() + System.lineSeparator() + "name: " + name;
-    }
+    public abstract String toString() ;
+
 }
+
 class Rogue extends Protagonist {
     public Rogue(String name) {
-	super(name);
-	hp = 75;
-    	default_attack = attack = 0.8;
+    	super(name);
+    	hp = 75;
+        default_attack = attack = 0.8;
     }
     
     public String toString() {
-        return super.toString() + System.lineSeparator() + "type: Rogue";
+        return  "name: " + getName() 
+                + System.lineSeparator() 
+                + "type: Rogue"
+                + System.lineSeparator()
+                + "HP: " + hp
+                + System.lineSeparator() 
+                + "strength: " + strength
+                + System.lineSeparator()
+                + "defense: " + defense
+                + System.lineSeparator()
+                + "attack: " + attack;
     }
 }
-class Paladin extends Protagonist {
-        public Paladin(String name) {
-	super(name);
-    	hp = 250;
-    	strength = 80;
-    }
+
+// class Paladin extends Protagonist {
+//         public Paladin(String name) {
+// 	super(name);
+//     	hp = 250;
+//     	strength = 80;
+//     }
     
-    public String toString() {
-        return super.toString() + System.lineSeparator() + "type: Paladin";
-    }
-}
-class Mage extends Protagonist {
-        public Mage(String name) {
-	super(name);
-    	hp = 80;
-    	default_defense = defense = 60;
-    }
+//     // public String toString() {
+//     //     return super.toString() + System.lineSeparator() + "type: Paladin";
+//     // }
+// }
+
+// class Mage extends Protagonist {
+//         public Mage(String name) {
+// 	super(name);
+//     	hp = 80;
+//     	default_defense = defense = 60;
+//     }
 	
-    public String toString() {
-        return super.toString() + System.lineSeparator() + "type: Mage";
-    }
-}
+//     // public String toString() {
+//     //     return super.toString() + System.lineSeparator() + "type: Mage";
+//     // }
+// }
+
+
+
+
+
+
+
+
+
